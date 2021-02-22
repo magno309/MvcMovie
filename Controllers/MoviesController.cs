aa05258d454c982b25cd2c94dba3a10afe26f37d
magno309 <48113838+magno309@users.noreply.github.com>
2021-02-22 15:28:13 -0600

    Terminada "Parte 10. Examen de los métodos Details y Delete de una aplicación de ASP.NET Core"

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -141,7 +141,7 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, bool notUsed)
         {
             if (id == null)
             {
